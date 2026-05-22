e6405ca266fcbdb7b4133dc4ad3375d2d9b403c1
Abrorbek Ismoilov <abror2142@gmail.com>
2026-05-22 11:57:19 +0500

    Fixing slug genration

--- bundle/WebDev/BlogBundle/src/Service/SlugGenerator.php
+++ bundle/WebDev/BlogBundle/src/Service/SlugGenerator.php
@@ -24,7 +24,7 @@ class SlugGenerator implements SlugGeneratorInterface
             $slug = $baseSlug . '-' . $suffix++;
         }
 
-        return uniqid($title, true);
+        return $slug;
     }
 
 }
